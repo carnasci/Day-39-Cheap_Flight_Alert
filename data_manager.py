9990607680fc981b2ff6c71e1b6ca8f3f1a750e8
carnasci <cen.master24@gmail.com>
2025-03-11 12:36:55 -0500

    Day 39 - project repo created

---
+++ data_manager.py
@@ -0,0 +1,10 @@
+import requests
+from credentials import sheet_auth
+
+
+SHEET_URL = "https://api.sheety.co/acce13247ad2cc699293ff3d1ff26f90/cheapFlightsAlert/prices"
+SHEET_AUTH = sheet_auth
+
+class DataManager:
+
+    pass
