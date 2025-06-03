ed44c1d891450eb033b5db9532dff2f95c721305
carnasci <cen.master24@gmail.com>
2025-06-03 08:42:38 -0500

    Day 39 - started work on check flights class, still need to specify times and need to go back and find the euro symbol and change it do dollar symbol

--- flight_search.py
+++ flight_search.py
@@ -1,17 +1,42 @@
 import os
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
+
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
 
 class FlightSearch:
 
     def __init__(self):
+        """
+                Initialize an instance of the FlightSearch class.
+                This constructor performs the following tasks:
+                1. Retrieves the API key and secret from the environment variables 'AMADEUS_API_KEY'
+                and 'AMADEUS_SECRET' respectively.
+                Instance Variables:
+                _api_key (str): The API key for authenticating with Amadeus, sourced from the .env file
+                _api_secret (str): The API secret for authenticating with Amadeus, sourced from the .env file.
+                _token (str): The authentication token obtained by calling the _get_new_token method.
+        """
         self.api_key = os.environ["AMADEUS_KEY"]
         self.api_secret = os.environ["AMADEUS_SECRET"]
+        # Getting a new token every time program is run. Could reuse unexpired tokens as an extension.
         self._token = self._get_new_token()
 
     def _get_new_token(self):
+        """
+                Generates the authentication token used for accessing the Amadeus API and returns it.
+                This function makes a POST request to the Amadeus token endpoint with the required
+                credentials (API key and API secret) to obtain a new client credentials token.
+                Upon receiving a response, the function updates the FlightSearch instance's token.
+                Returns:
+                    str: The new access token obtained from the API response.
+        """
         header = {
             "Content-Type" : "application/x-www-form-urlencoded"
         }
@@ -25,10 +50,11 @@ class FlightSearch:
             headers=header,
             data=body,
             )
-        data = response.json()
-        print(f"Your token is {data['access_token']}")
-        print(f"Your token expires in {data['expires_in']} seconds")
-        return data["access_token"]
+
+        # New bearer token. Typically expires in 1799 seconds (30min)
+        print(f"Your token is {response.json()['access_token']}")
+        print(f"Your token expires in {response.json()['expires_in']} seconds")
+        return response.json()["access_token"]
 
     def get_iataCode(self, city_name):
         """
@@ -49,7 +75,7 @@ class FlightSearch:
                 to a KeyError), it logs a message indicating that no airport code was found for the city
                 and returns "Not Found".
                 """
-
+        print(f"Using this token to get destination code {self._token}")
         api_endpoint = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
         header = {
             "Authorization" : f"Bearer {self._token}"
@@ -76,3 +102,51 @@ class FlightSearch:
             print(f"KeyError: No airport code found for {city_name}.")
             return "Not Found"
         return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        """
+                Searches for flight options between two cities on specified departure and return dates
+                using the Amadeus API.
+                Parameters:
+                    origin_city_code (str): The IATA code of the departure city.
+                    destination_city_code (str): The IATA code of the destination city.
+                    from_time (datetime): The departure date.
+                    to_time (datetime): The return date.
+                Returns:
+                    dict or None: A dictionary containing flight offer data if the query is successful; None
+                    if there is an error.
+                The function constructs a query with the flight search parameters and sends a GET request to
+                the API. It handles the response, checking the status code and parsing the JSON data if the
+                request is successful. If the response status code is not 200, it logs an error message and
+                provides a link to the API documentation for status code details.
+        """
+        # print(f"Using this token to check_flights() {self._token}")
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": "USD",
+            "max": "10",
+        }
+
+        response = requests.get(
+            url=FLIGHT_ENDPOINT,
+            headers=headers,
+            params=query,
+        )
+
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            print("There was a problem with the flight search.\n"
+                  "For details on status codes, check API documentation:\n"
+                  "https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api"
+                  "-reference")
+            print(f"Response body:", response.text)
+            return None
+
+        return response.json()
+
