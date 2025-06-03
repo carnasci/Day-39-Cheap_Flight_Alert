f1f4f5b3ca6fcdb76e65918a054236be271a3fd9
carnasci <cen.master24@gmail.com>
2025-06-03 06:53:50 -0500

    Day 39 - had to take a long break, slowly trying to finish up capstone project, flight_data class finished

--- flight_search.py
+++ flight_search.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class FlightSearch:
+
     def __init__(self):
         self.api_key = os.environ["AMADEUS_KEY"]
         self.api_secret = os.environ["AMADEUS_SECRET"]
@@ -30,6 +31,25 @@ class FlightSearch:
         return data["access_token"]
 
     def get_iataCode(self, city_name):
+        """
+                Retrieves the IATA code for a specified city using the Amadeus Location API.
+                Parameters:
+                city_name (str): The name of the city for which to find the IATA code.
+                Returns:
+                str: The IATA code of the first matching city if found; "N/A" if no match is found due to an IndexError,
+                or "Not Found" if no match is found due to a KeyError.
+
+                The function sends a GET request to the IATA_ENDPOINT with a query that specifies the city
+                name and other parameters to refine the search. It then attempts to extract the IATA code
+                from the JSON response.
+                - If the city is not found in the response data (i.e., the data array is empty, leading to
+                an IndexError), it logs a message indicating that no airport code was found for the city and
+                returns "N/A".
+                - If the expected key is not found in the response (i.e., the 'iataCode' key is missing, leading
+                to a KeyError), it logs a message indicating that no airport code was found for the city
+                and returns "Not Found".
+                """
+
         api_endpoint = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
         header = {
             "Authorization" : f"Bearer {self._token}"
